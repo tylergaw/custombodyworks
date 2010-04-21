9262270bc6a612bdc7de7498e5ce45dba9bc5b0c
Tyler Gaw <me@tylergaw.com>
2010-04-21 12:54:37 +0000

    Adding quote. Bio styles

--- about.php
+++ about.php
@@ -9,21 +9,30 @@
 		</p>
 	</div>
 	<div id="bio">
-		<h3>Karma Gaw</h3>
-		<img src="images/karmagaw.jpg" alt="Karma Gaw" />
-		<p>
-			...is a Nationally Certified Licensed Massage Therapist, Licensed Esthetician, and Certified Personal Trainer. As a Personal Trainer she has certified over 45 women to be instructors in the fitness industry. Her background for bodyworks spans 20 years and growing as a personal trainer with numerous clients, a Certified Aerobic, Step, and Kickboxing Instructor. In the last 20 years she has also competed in National Aerobic Championships and has appeared in American Fitness magazine twice for fitness training..
-		</p>
-		<p>
-			Karma believes that she was meant to be "Good Karma" through touch. She is very intuitive and truly believes massage allows the body to heal itself. Karma's stronger modalities are <a href="services.php#trigger-point">Trigger Point</a> and <a href="services.php#reflexology">Foot Reflexology</a>. Although all of her techniques are well practiced she will continue her education.
-		</p>
-		<p>
-			Karma has been practicing at her Bowling Green, Mo location for 2 years. She also works with two hospice services in the area. Her work has become more customized and therapeutic, educating her clients to live with wellness instead of pain and stress.
-		</p>
+		<div class="aside">
+			<img src="images/karmagaw.jpg" alt="Karma Gaw" />
+			<blockquote>
+				<p>
+					I truly believe that the work I do is a gift and it is a pleasure to pass that gift on to my clients.
+				</p>
+			</blockquote>
+		</div>
+		<div class="section">
+			<h3>Karma Gaw</h3>
+			<p>
+				...is a Nationally Certified Licensed Massage Therapist, Licensed Esthetician, and Certified Personal Trainer. As a Personal Trainer she has certified over 45 women to be instructors in the fitness industry. Her background for bodyworks spans 20 years and growing as a personal trainer with numerous clients, a Certified Aerobic, Step, and Kickboxing Instructor. In the last 20 years she has also competed in National Aerobic Championships and has appeared in American Fitness magazine twice for fitness training.
+			</p>
+			<p>
+				Karma believes that she was meant to be "Good Karma" through touch. She is very intuitive and truly believes massage allows the body to heal itself. Karma's stronger modalities are <a href="services.php#trigger-point">Trigger Point</a> and <a href="services.php#reflexology">Foot Reflexology</a>. Although all of her techniques are well practiced she will continue her education.
+			</p>
+			<p>
+				Karma has been practicing at her Bowling Green, Mo location for 2 years. She also works with two hospice services in the area. Her work has become more customized and therapeutic, educating her clients to live with wellness instead of pain and stress.
+			</p>
+		</div>
 		<dl>
 			<dt>Accreditations &amp; Memberships</dt>
 			<dd><acronym title="Nationally Certified Therapist of Massage and Bodyworks">NCTMB</acronym> #2006012494</dd>
-			<dd>Licensed Esthetician <em>(skin care specialist)</em> #2004029339</dd>
+			<dd>Licensed Esthetician #2004029339 <em>(skin care specialist)</em></dd>
 			<dd><acronym title="Aerobics and Fitness Association of America">AFAA</acronym> Certified Personal Trainer PT #592-3</dd>
 			<dd><acronym title="Associated bodywork &amp; massage professionals">ABMP</acronym> Member</dd>
 		</dl>
